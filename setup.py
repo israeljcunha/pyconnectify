38e7fb595f38a1faef75e4641ea22c4613d4b0db
Israel Cunha <israelcunhamail@gmail.com>
2020-01-29 22:17:42 -0300

    Setting setup and configs and new upload

--- setup.py
+++ setup.py
@@ -1,40 +1,43 @@
-# import setuptools
+# from setuptools import setup, find_packages
 #
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
-#
-# setuptools.setup(
-#     name="pyconnectify",
-#     version="0.1",
-#     author="Israel Cunha",
-#     author_email="ms.israel.cunha@gmail.com",
-#     description=" ... ",
-#     long_description=long_description,
+# setup(
+#     name='pyconnectify',
+#     version='0.0.3',
+#     url='https://gitlab.com/israelcunha/PyConnectify',
+#     license='MIT License',
+#     author='Israel Cunha',
+#     author_email='ms.israel.cunha@gmail.com',
+#     keywords='ssh utp tcp tar redes network',
+#     description=u'Telecommunication and file management application',
+#     packages=find_packages(exclude=['venv']),
 #     long_description_content_type="text/markdown",
-#     url="https://gitlab.com/israelcunha/PyConnectify",
-#     packages=setuptools.find_packages(exclude=['venv']),
-#     classifiers=(
-#         "Programming Language :: Python :: 3.7.5",
-#         "License :: OSI Approved :: MIT License",
-#         "Operating System :: OS Independent",
-#     ),
-#     zip_safe=False,
-#     setup_requires=['nose>=1.0'],
+#     long_description=open('README.md').read(),
+#     install_requires=[],
 # )
 
-from setuptools import setup
+import setuptools
 
-setup(
-    name='pyconnectify',
-    version='0.0.2.1',
-    url='https://gitlab.com/israelcunha/PyConnectify',
-    license='MIT License',
-    author='Israel Cunha',
-    author_email='ms.israel.cunha@gmail.com',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyconnectify",
+    version="0.0.3.1",
+    author="Israel Cunha",
+    author_email="ms.israel.cunha@gmail.com",
     keywords='ssh utp tcp tar redes network',
-    description=u'Telecommunication and file management application',
-    packages=['pyconnectify'],
+    description="Telecommunication and file management application",
+    license='MIT License',
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    long_description=open('README.md').read(),
-    install_requires=[],
+    url="https://gitlab.com/israelcunha/PyConnectify",
+    packages=setuptools.find_packages(exclude=['venv']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
